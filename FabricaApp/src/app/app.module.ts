a7cc2dee6cdd0dc6c316450d8a22c895b934522f
FREYMAN ANDRES REYES ANAYA <frerey31@gmail.com>
2025-04-28 20:54:04 -0500

    subiendo a la 18

--- FabricaApp/src/app/app.module.ts
+++ FabricaApp/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { LoginModule } from './auth/login/login.module';
 import { RegisterModule } from './auth/register/register.module';
@@ -12,23 +12,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from './shared/layout/layout.module';
 
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    ToastrModule.forRoot(),
-    LoginModule,
-    RegisterModule,
-    BrowserAnimationsModule,
-    LayoutModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        ToastrModule.forRoot(),
+        LoginModule,
+        RegisterModule,
+        BrowserAnimationsModule,
+        LayoutModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
